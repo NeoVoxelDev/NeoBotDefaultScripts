c6261fda02a3b0da828a4e0e53775e1757be2826
Aurelian2842 <aurelian2842@gmail.com>
2025-11-24 18:22:51 +0800

    optimize: remove undefined/null return for parsing text message

--- util/util.js
+++ util/util.js
@@ -26,7 +26,7 @@ class BetterQQ {
       if (item.type === "text") {
         newMessage += item.data.text
       } else {
-        return undefined
+        return ""
       }
     }
     return newMessage
